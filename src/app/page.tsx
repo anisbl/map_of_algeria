8ce8d264a1dd4da9ea56b34d70e98a278ab20107
unknown <a_belaiouar@estin.dz>
2025-02-18 14:53:00 +0100

    style the main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,17 @@
 import AlgeriaMap from "./AlgeriaMap";
-// import AlgeriaMap2 from "./AlgeriaMapp";
+
 export default function Home() {
   return (
-    <div className="App w-[95vw] h-full flex flex-col justify-center items-center "> 
-    <h1>Algeria Map 58 wilaya by anisbl </h1>
-    <div className="flex w-[80vw] h-[80vh] ">
-      <AlgeriaMap />
-    
+    <div className="App w-full h-full flex flex-col justify-center items-center p-4">
+      
+      <h1 className="text-3xl md:text-4xl font-semibold text-center mb-6">
+        Algeria Map - 58 Wilayas by Anisbl
+      </h1>
+
+     
+      <div className="flex justify-center items-center w-full h-[60vh] md:h-[80vh] bg-gray-100 p-4 rounded-lg shadow-lg">
+        <AlgeriaMap />
+      </div>
     </div>
-    
-  </div>
   );
 }
